022add4f23506f59590f74057e8a83e30724ca62
Karim Youssef <127368253+kkarimyyoussef@users.noreply.github.com>
2026-06-22 18:40:39 +0300

    Time: 595 ms (27.32%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3910-find-books-with-no-available-copies/find-books-with-no-available-copies.sql
@@ -0,0 +1,20 @@
+SELECT 
+    l.book_id,
+    l.title,
+    l.author,
+    l.genre,
+    l.publication_year,
+    COUNT(b.record_id) AS current_borrowers
+FROM library_books l
+JOIN borrowing_records b
+    ON l.book_id = b.book_id
+WHERE b.return_date IS NULL
+GROUP BY 
+    l.book_id, # this is unqiue to the book only this is logically needed
+    l.title, # the rest is inluded to obey sql (ONLY_FULL_GROUP_BY) rule.
+    l.author,
+    l.genre,
+    l.publication_year,
+    l.total_copies
+HAVING COUNT(b.record_id) = l.total_copies
+ORDER BY current_borrowers DESC, l.title;
