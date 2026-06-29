8fbbbc9f36aec0eed8d36fe3b817d152ce8889ee
Karim Youssef <127368253+kkarimyyoussef@users.noreply.github.com>
2026-06-30 02:44:57 +0300

    Time: 775 ms (41.51%) | Memory: 0B (100.00%) - LeetSync

---
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+WITH Ranked as(
+    SELECT 
+        Department.name AS Department,
+        Employee.name AS Employee,
+        Employee.salary As Salary,
+    DENSE_RANK() OVER (PARTITION BY Department.id ORDER BY Employee.salary DESC) AS rn
+    FROM Department
+    JOIN Employee
+    ON Department.id = Employee.departmentid
+)
+
+SELECT Department, Employee, Salary
+FROM Ranked
+Where rn = 1;
